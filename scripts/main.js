48c1efedd008b84b1566b475a39b2ee31ae8916f
Will Estanislao <72217283+estanislaowillia@users.noreply.github.com>
2023-09-05 12:27:16 -0400

    Added a function to randomize symbol after card has been guessed

--- scripts/main.js
+++ scripts/main.js
@@ -2,67 +2,66 @@
  * JS script
  */
 
+const Images = {
+  Cross: "images/card-symbols/cross.jpg",
+  Circle: "images/card-symbols/circle.jpg",
+  Wave: "images/card-symbols/wave.jpg",
+  Square: "images/card-symbols/square.jpg",
+  Star: "images/card-symbols/star.jpg",
+};
+
 const userHand = document.getElementById("user-card-choices");
 const cardOptions = userHand.getElementsByClassName("zener-card");
 let cardToGuess = document.getElementById("hidden-card");
 let gameText = document.getElementById("card-reveal");
 let scoreText = document.getElementById("player-score");
+let hiddenCard = document.getElementById("hidden-symbol");
 
 let isCardHidden = true;
 let playerScore = 0;
-/*
+
 for (i = 0; i < cardOptions.length; i++) {
   cardOptions[i].addEventListener("click", (e) => {
     checkCardChoice(e.target);
   });
 }
-*/
-
-const checkCardChoice2 = (e) => {
-  let hiddenCard = document.getElementById("hidden-symbol");
-  if (hiddenCard.src == e.target.src) {
-    ++playerScore;
-    isCardHidden = false;
-
-    // Do animation flip
-    gameText.innerHTML = "You guessed correctly!";
-    scoreText.innerHTML = "Score: " + playerScore;
-  } else {
-    gameText.innerHTML = "You guessed incorrectly!";
-    isCardHidden = false;
-  }
-  logStats();
-  console.log(e.target.src);
-  console.log(hiddenCard.src);
-};
-
-for (let choice of cardOptions) {
-  choice.addEventListener("click", checkCardChoice2);
-}
 
 // User picks a card
 // Card is checked against current
 function checkCardChoice(e) {
-  let hiddenCard = document.getElementById("hidden-symbol");
   if (hiddenCard.src == e.src) {
     ++playerScore;
     isCardHidden = false;
 
-    // Do animation flip
     gameText.innerHTML = "You guessed correctly!";
     scoreText.innerHTML = "Score: " + playerScore;
   } else {
     gameText.innerHTML = "You guessed incorrectly!";
     isCardHidden = false;
   }
+
+  // Do animation flip of card
+  // Wait 15s - 30s before randomizing card
   logStats();
   console.log(e.src);
   console.log(hiddenCard.src);
+
+  changeHiddenCard();
 }
 
 function logStats() {
   console.log("Current Player Score: " + playerScore);
   console.log("Is Card Hidden? " + isCardHidden);
+}
+
+function changeHiddenCard() {
+  let randomNumber = Math.floor(Math.random() * 5);
+  let cardSymbolKeys = Object.keys(Images);
+  hiddenCard.src = Images[cardSymbolKeys[randomNumber]];
+
+  console.log("Random Number: " + randomNumber);
+  console.log("Card Symbol Key: " + cardSymbolKeys);
+  console.log(hiddenCard.src);
 }
 
 // Event
